7e13347c3e90bc27751a7e0b7f5d6d53e1f7a306
mame06 <june.imifu@gmail.com>
2020-11-13 14:57:17 +0900

    edit pm2

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">むずかしいね！</a>
+          Welcome to <a href="https://nextjs.org">むずかしいわよ！</a>
         </h1>
 
         <p className={styles.description}>
